f8d49c569b575cdd2d0c53d81c1cb73a145eede3
STG7 <shauryaggamer@gmail.com>
2022-10-10 18:52:51 -0700

    asdf

--- python_code/apitest.py
+++ python_code/apitest.py
@@ -1,7 +1,40 @@
-import requests
+import requests, json
 
-url = "http://www.randomnumberapi.com/api/v1.0/random?min=100&max=1000&count=5"
 
-response = requests.request("GET", url)
+url = "https://us-zip-code-information.p.rapidapi.com/"
 
-print(response.text)
+querystring = {"zipcode":"92127"}
+
+headers = {
+	"X-RapidAPI-Key": "6cef6d9f81mshf9a1793ed8d336ap1c4e09jsn658eb44a2a5f",
+	"X-RapidAPI-Host": "us-zip-code-information.p.rapidapi.com"
+}
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+locations = json.loads(response.text)
+lat = ""
+lon = ""
+for location in locations:
+  lat =  location["Latitude"]
+  lon = location["Longitude"]
+
+
+  url = "https://weatherbit-v1-mashape.p.rapidapi.com/current"
+
+querystring = {"lon":lon,"lat":lat, "units": 'imperial'}
+
+headers = {
+	"X-RapidAPI-Key": "6cef6d9f81mshf9a1793ed8d336ap1c4e09jsn658eb44a2a5f",
+	"X-RapidAPI-Host": "weatherbit-v1-mashape.p.rapidapi.com"
+}
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+json_object = json.loads(response.text)
+
+json_formatted_str = json.dumps(json_object, indent=2)
+
+print(json_formatted_str)
+
+for tempvalue in json_object.get("data"):
+    print(tempvalue["app_temp"])
